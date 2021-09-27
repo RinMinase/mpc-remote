d0d655d673ae2d03a2c8acf3e0c71427ce4837ef
Rin Minase <rin.black.raison.detre@gmail.com>
2021-09-27 23:42:19 +0800

    Appended UI

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,9 +1,8 @@
 import { default as React, useEffect } from "react";
+import c from "clsx";
 
 import style from "./index.scss";
-import {
-	getUser,
-} from "./actions";
+import { getUser } from "./actions";
 
 export default function Home() {
 	useEffect(async () => {
@@ -11,11 +10,89 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div>
-			<p className={style.test}>Home Component loaded!</p>
-			<div className="spinner round"></div>
+		<div className={style.container}>
+			<div className={style.headerContainer}>
+				<p className={style.header}>
+					Filename - Very Long Long Long Long
+					Long Long Long Long Long Long Filename
+				</p>
+			</div>
 
-			<p>SHA IDs</p>
+			<div className={style.close}>
+				<button className={c(style.button, style.closeButton)}>CLOSE</button>
+			</div>
+
+			<div className={style.col6}>
+				<button className={style.button}>
+					<i class="material-icons">undo</i>
+				</button>
+			</div>
+
+			<div className={style.col6}>
+				<button className={style.button}>
+					<i class="material-icons">redo</i>
+				</button>
+			</div>
+
+			<div className={style.playButtonContainer}>
+				<button className={style.playButton}>
+					<i className={"material-icons " + style.playIcon}>play_arrow</i>
+					{/* <i class="material-icons">pause</i> */}
+				</button>
+			</div>
+
+			<div className={c(style.col6, style.fwdRwdContainer)}>
+				<button className={style.button}>
+					<i class="material-icons">fast_rewind</i>
+				</button>
+			</div>
+
+			<div className={c(style.col6, style.fwdRwdContainer)}>
+				<button className={style.button}>
+					<i class="material-icons">fast_forward</i>
+				</button>
+			</div>
+
+			<div className={c(style.col4, style.volumeBtnContainer)}>
+				<button
+					className={c(
+						style.button,
+						style.volumeButton,
+						style.volumeButtonMinus
+					)}
+				>
+					<i class="material-icons">remove</i>
+				</button>
+			</div>
+
+			<div className={c(style.col4, style.volumeBtnContainer)}>
+				<button
+					className={c(
+						style.button,
+						style.volumeButton,
+						style.volumeButtonMute
+					)}
+				>
+					<i class="material-icons">volume_off</i>
+					{/* <i class="material-icons">volume_up</i> */}
+				</button>
+			</div>
+
+			<div className={c(style.col4, style.volumeBtnContainer)}>
+				<button
+					className={c(
+						style.button,
+						style.volumeButton,
+						style.volumeButtonPlus
+					)}
+				>
+					<i class="material-icons">add</i>
+				</button>
+			</div>
+
+			<div className={style.volumeSliderContainer}>
+				{/* <input type="range" min="1" max="100" value="0" className={style.volumeSlider} /> */}
+			</div>
 		</div>
 	);
 }
