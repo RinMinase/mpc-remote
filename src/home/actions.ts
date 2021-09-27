5d639d94b3be322436ef2933c3064a7599dc1b56
Rin Minase <rin.black.raison.detre@gmail.com>
2021-09-28 00:44:19 +0800

    Modified play function to use env

--- src/home/actions.ts
+++ src/home/actions.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
 import qs from "qs";
 
-// const MPC_BASE_DOMAIN = process.env.MPC_BASE_DOMAIN || "localhost";
-// const MPC_BASE_PORT = process.env.MPC_BASE_PORT || "13579";
-const MPC_BASE_DOMAIN = "localhost";
-const MPC_BASE_PORT = "13579";
+const MPC_BASE_DOMAIN = process.env.MPC_BASE_DOMAIN || "localhost";
+const MPC_BASE_PORT = process.env.MPC_BASE_PORT || "13579";
 const MPC = `http://${MPC_BASE_DOMAIN}:${MPC_BASE_PORT}/command.html`;
 
 const config = {
 	headers: {
 		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-		"Content-Type": "application/x-www-form-urlencoded"
+		"Content-Type": "application/x-www-form-urlencoded",
 	}
 }
 
