689e876c2c7c23864cbdfc8b352c9b5737507758
Rin Minase <rin.black.raison.detre@gmail.com>
2022-09-05 14:58:12 +0800

    Fixed build issues

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { Configuration } from "webpack";
+import "webpack-dev-server";
 
 import Dotenv from "dotenv-webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
